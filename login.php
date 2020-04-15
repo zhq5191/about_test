6ea7230c5c28d64ac9643562dad53983e309026e
zhanghaiqiang <89786632@qq.com>
2020-04-15 15:14:24 +0800

    修改login.php

--- login.php
+++ login.php
@@ -3,12 +3,12 @@
 // 告诉浏览器返回为json类型
 header('Content-Type:application/json; charset=utf-8');
 
+// 较密用的key
+$key = 'zhqzs';
 
 // 获取参数
-
 $name = $_GET['name'];
 $pwd = $_GET['pwd'];
-
 
 /**
  * 在真正的开发过程中，会操作数据库，我们暂时用一个多维数组代替
@@ -18,15 +18,19 @@ $users = array(
     array('name' => 'tom','pwd' => '654321', 'nick' => '帅的不明显')
 );
 
+$tokens = array(
+    'zhq' => 'db16f83919890274272f4517fbba9d76',
+    'tom' => 'e10adc3949ba59abbe56e057f20f883e'
+);
+
 foreach($users as $key=>$value){
 
     if($value['name']==$name && $value['pwd']==$pwd){
         // 一通骚操作后，给$token赋值
-        $token = 'db16f83919890274272f4517fbba9d76';
+        $token = $tokens[$name];    // 看门大爷登记，发证
         $nick_name = $value['nick'];
     }
 }
-
 
 // 组织的数据
 $data = array(
@@ -35,7 +39,7 @@ $data = array(
     'result' => array(
         'name' => $name,
         'nick' => $nick_name,
-        'token' => $token
+        'token' => $token,
     ),
 
 );
